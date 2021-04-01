4107f22c0b1035399532b85e88d4611ad7a2ab14
dci <rudeshdhote@gmail.com>
2021-04-01 15:16:46 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,37 @@
+// Lowest Number
+
+console.log("The min: " + Math.min(-1, 4))
+
+// Highest Number
+
+console.log("The max: " + Math.max(-1, 4))
+
+// Round up
+
+
+
+console.log("Ceil: " + Math.ceil(3321.32321));
+
+console.log("Ceil: " + Math.ceil(326.76));
+
+console.log("Ceil: " + Math.ceil(76788.7));
+
+console.log("Ceil: " + Math.ceil(-9.78));
+
+console.log("Ceil: " + Math.ceil(43.342));
+
+// Round down
+
+console.log("Floor: " + Math.floor(3321.32321));
+
+console.log("Floor: " + Math.floor(326.76));
+
+console.log("Floor: " + Math.floor(76788.7));
+
+console.log("Floor: " + Math.floor(-9.78));
+
+console.log("Floor: " + Math.floor(28.329));
+
+// Dice Roll!
+
+console.log(Math.ceil(Math.random() * 6));
